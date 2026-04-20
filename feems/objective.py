88a50af2f61153e74c30dc0d3957355f15678ab6
Clemens Schmid <nevromecs@gmail.com>
2026-04-20 17:25:58 +0200

    decided to compute the fitted distances between all nodes slightly differently; should only have an effect on sampled nodes

--- feems/objective.py
+++ feems/objective.py
@@ -278,13 +278,18 @@ def comp_mats_all_nodes(obj, rtol=1e-10):
     # full pseudo-inverse of Laplacian
     Linv_all_nodes = pinvh(L, rtol=rtol)
 
-    # construct Q^{-1} on full node set
-    Qinv_all_nodes = np.zeros((d, d))
-    Qinv_all_nodes[:o, :o] = sp_graph.q_inv_diag.toarray()
-
     # fitted covariance
     one = np.ones((d, d)) / d
-    fit_cov_all_nodes = Linv_all_nodes - one + Qinv_all_nodes
+    fit_cov_all_nodes = Linv_all_nodes - one
+
+    # Qinv is an extra variance term only available for the observed nodes.
+    # Adding it yields the same fitted covariances as in comp_mats.
+    # Here we're interested in the latent spatial covariance/distance induced
+    # by migration alone, so we omit it.
+    # To add it anyway:
+    #Qinv_all_nodes = np.zeros((d, d))
+    #Qinv_all_nodes[:o, :o] = sp_graph.q_inv_diag.toarray()
+    #fit_cov_all_nodes = Linv_all_nodes - one + Qinv_all_nodes
 
     # recover original node order
     permuted_idx = np.array(
